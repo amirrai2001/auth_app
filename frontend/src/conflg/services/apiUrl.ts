14da47ee2fa60264f8921f8c89dbcc264e028038
amirrai2001 <109527539+amirrai2001@users.noreply.github.com>
2025-04-07 23:16:28 +0530

    Update apiUrl.ts
    railway url for baceknd

--- frontend/src/conflg/services/apiUrl.ts
+++ frontend/src/conflg/services/apiUrl.ts
@@ -1,2 +1,2 @@
-export const BASEURL = 'http://13.126.203.18:5000/api/'
-export const BASEURLPORT = `http://13.126.203.18:5000/`
+export const BASEURL = 'https://authapp-production-268c.up.railway.app/app/'
+export const BASEURLPORT = `https://authapp-production-268c.up.railway.app/`
