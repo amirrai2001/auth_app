a834a2c0ad1383ac761c01016a884922f5ba5597
amirrai2001 <109527539+amirrai2001@users.noreply.github.com>
2025-04-07 23:24:00 +0530

    Update apiUrl.ts

--- frontend/src/conflg/services/apiUrl.ts
+++ frontend/src/conflg/services/apiUrl.ts
@@ -1,2 +1,2 @@
-export const BASEURL = 'https://authapp-production-268c.up.railway.app/app/'
-export const BASEURLPORT = `https://authapp-production-268c.up.railway.app/`
+export const BASEURL = 'https://authapp-production-268c.up.railway.app/api/'
+export const BASEURLPORT = `https://authapp-production-268c.up.railway.api/`
