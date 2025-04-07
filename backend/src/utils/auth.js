40dffa58c76fda8f4821c1d95711808ee40b2dfe
amirrai2001 <109527539+amirrai2001@users.noreply.github.com>
2025-04-08 00:45:49 +0530

    Update auth.js

--- backend/src/utils/auth.js
+++ backend/src/utils/auth.js
@@ -6,8 +6,9 @@ const sendToken = (user, statusCode, res) => {
     });
   
     const cookieOptions = {
-      httpOnly: true,
-      domain:'auth-app-psi-three.vercel.app',
+  httpOnly: true,
+      secure: process.env.NODE_ENV === 'production', // Only over HTTPS
+      sameSite: 'None', 
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     };
   
